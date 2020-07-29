e43f8e809c9dd908d6e4fbe8a0b77b37ea282ce6
Murata Mitsuharu <hikari.photon+mygit@gmail.com>
2020-07-29 15:38:48 +0900

    new

---
+++ assets/js/rand_color.js
@@ -0,0 +1,17 @@
+class ColorTheme{}
+
+function rand_color(){
+    let req = new XMLHttpRequest
+    req.open("GET", "/assets/json/color_list.json")
+    req.send()
+    req.onload = function(e){
+        ColorTheme.palette = JSON.parse(req.response)
+        let color_name = Object.keys(ColorTheme.palette)[parseInt(Math.random(Object.keys(ColorTheme.palette).length))]
+        let color, light, dark
+        [color, light, dark] = ColorTheme.palette[color_name]
+        let style = document.createElement("style")
+        style.innerHTML = `body{ --color: ${color}; --light: ${light}; --dark: ${dark}; }`
+        document.head.append(style)
+    }
+}
+rand_color()
---
+++ test.md
@@ -0,0 +1,20 @@
+---
+---
+
+<link rel="stylesheet" href="/assets/css/default_theme.css">
+
+<button>ボタン</button>
+
+<input type="button" value="input">
+
+<input type="text" value="text">
+
+[localhost](localhost)
+
+# 開始
+お
+## 開始
+### 開始
+#### 開始
+##### 開始
+###### 開始
